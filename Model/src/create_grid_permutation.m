582e83f8ab3fb50b4d94c17b0712b30e28c97587
Ted <teddychenster@gmail.com>
2023-06-07 10:36:06 +0800

    Change grid search order of parameter permutation

--- Model/src/create_grid_permutation.m
+++ Model/src/create_grid_permutation.m
@@ -1,6 +1,6 @@
 
     function grid_permutaion = create_grid_permutation(betas, delta_1s, delta_2s)
-        [Beta, Delta_1, Delta_2] = meshgrid(betas, delta_1s, delta_2s);
+        [Delta_1, Delta_2, Beta] = meshgrid(delta_1s, delta_2s, betas);
 
         % Reshape the arrays into column vectors
         Beta = reshape(Beta, [], 1);
